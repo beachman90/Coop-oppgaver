95817fc51fe5f0af345f95a1a48b264308c18363
Jan Inge <jistrandman@gmail.com>
2024-02-13 10:38:44 +0100

    Laget løsningsforslag for oppgave 3

---
+++ New folder/Oppgave 3/Oppgave 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Dynamisk path
+        string directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+        try
+        {
+            // Bruker Directory.GetFiles med SearchOption.AllDirectories for å hente alle .txt filer fra alle undermapper
+            string[] txtFiles = Directory.GetFiles(directoryPath, "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("Sjekker .txt filer for 'sommer':");
+            foreach (string file in txtFiles)
+            {
+                // Leser innholdet i hver fil
+                string content = File.ReadAllText(file);
+
+                // Sjekker om filene inneholder "sommer"
+                if (content.Contains("sommer", StringComparison.OrdinalIgnoreCase)) // Ignorerer om det er store/små bokstaver
+                {
+                    Console.WriteLine($"{file} inneholder 'sommer'");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"En feil oppstod: {ex.Message}");
+        }
+    }
+}
